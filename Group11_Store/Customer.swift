0ae28b802beecbaa9171d1a1a0fecd4ab622244a
Wiwat Thaphon <wiwat.thaphon@gmail.com>
2023-05-25 00:14:22 -0400

    Customer and Game not finish

---
+++ Group11_Store/Customer.swift
@@ -0,0 +1,32 @@
+//
+//  Customer.swift
+//  Group11_Store
+//
+//  Created by Wiwat Thaphon on 2023-05-25.
+//
+
+import Foundation
+
+class Customer {
+    
+    var itemList: String = ""
+    var isMultiplayer: String = ""
+    var cost: Double = 0.0
+    
+//    //initializer - initializes default values for class attributes
+//    init(flightNumber: String, distance: Double, departure: String, arrival: String) {
+//        self.flightNumber = flightNumber
+//        self.distance = distance
+//        self.departure = departure
+//        self.arrival = arrival
+//        self.cost = 1000 + (distance * 0.75)
+//    }
+//
+//    func getTripDetails() {
+//        print("Flight \(flightNumber), Departing \(departure), Arriving \(arrival). Distance: \(distance)")
+//    }
+//    
+//    func getTypeTravel() -> String {
+//        return "F"
+//    }
+}
---
+++ Group11_Store/Game.swift
@@ -0,0 +1,32 @@
+//
+//  Game.swift
+//  Group11_Store
+//
+//  Created by Wiwat Thaphon on 2023-05-25.
+//
+
+import Foundation
+
+class Flight {
+    
+    var publisher: String = ""
+    var isMultiplayer: String = ""
+    var cost: Double = 0.0
+    
+//    //initializer - initializes default values for class attributes
+//    init(flightNumber: String, distance: Double, departure: String, arrival: String) {
+//        self.flightNumber = flightNumber
+//        self.distance = distance
+//        self.departure = departure
+//        self.arrival = arrival
+//        self.cost = 1000 + (distance * 0.75)
+//    }
+//
+//    func getTripDetails() {
+//        print("Flight \(flightNumber), Departing \(departure), Arriving \(arrival). Distance: \(distance)")
+//    }
+//
+//    func getTypeTravel() -> String {
+//        return "F"
+//    }
+}
